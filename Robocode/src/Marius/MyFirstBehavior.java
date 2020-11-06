de1bf7bb4e768b6347c31bc202b7f363a628d70a
MariusWenk <Marius_Wenk@gmx.de>
2020-11-06 21:55:47 +0100

    Erste Scgritte (Scanner)

--- Robocode/src/Marius/MyFirstBehavior.java
+++ Robocode/src/Marius/MyFirstBehavior.java
@@ -3,23 +3,67 @@ import java.awt.Color;
 
 public class MyFirstBehavior extends SimpleRobotBehavior {
 	//Ged�chtnissektion
-	
+
+	double angle =  360;
+	boolean scanned = false;
+	double bearing;
+	double enemyDistance;
+	double enemyHeading;
+	double enemyVelocity;
+	double enemyEnergy;
+
 	public MyFirstBehavior(SimpleRobot  robot) {
 		super(robot);
 	}
-	
+
 	@Override
 	public void start() {
-		turnRadar(720);
+		turnRadar(360);
 	}
 
 	@Override
 	void execute() {
-		for(var e : getScannedRobotEvents()){
-
-		}
-		ahead(10);
+		scannerShooter();
+		move();
+		//ahead(10);
 	}
-	
-	//Eigene Funktionen Sektion
+
+	public void scannerShooter(){
+		double gunAngle = 0;
+		double power = 1;
+		for(var e : getScannedRobotEvents()){
+			//Scannereinstellung
+			bearing = e.getBearing();
+			double heading = getHeading();
+			double radarHeading = getRadarHeading();
+			double primAngle = Utils.normalRelativeAngle(heading-radarHeading+bearing);
+			if(primAngle > 0){
+				angle = primAngle+20;
+			}
+			else{
+				angle = primAngle-20;
+			}
+			//Guneinstellung
+			double gunHeading = getGunHeading();
+			enemyDistance = e.getDistance();
+			enemyHeading = e.getHeading();
+			enemyVelocity = e.getVelocity();
+			gunAngle = Utils.normalRelativeAngle(heading-gunHeading+bearing);
+			//Datenübergabe
+			enemyEnergy = e.getEnergy();
+			scanned = true;
+		}
+		//Scanner
+		turnRadar(angle);
+		//Gun
+		turnGun(gunAngle);
+		if(gunAngle <=2 && gunAngle >= -2){
+			fireBullet(power);
+		}
+	}
+
+	public void move(){
+
+	}
+
 }
